f1920d8b0058da0b021a0b605f9c7bbc1a8a8463
shubox <shumasod@gmai.com>
2024-05-08 23:47:27 +0900

    編集

--- new_repository/resources/views/app.blade.php
+++ new_repository/resources/views/app.blade.php
@@ -23,6 +23,7 @@
 
     <!-- Bootstrap JS -->
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
     <!-- Tailwind JS (if needed) -->
     <script src="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.js"></script>
